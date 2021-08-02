a3a084e54eb49e4776e834d64fc998f3c792e9c3
Fabien <fabienthiot@gmail.com>
2021-08-02 19:28:04 +0200

    Homepage photograph filter + template string

--- js/indexgridcell.js
+++ js/indexgridcell.js
@@ -1,118 +1,46 @@
 // Création des GridCell (page d'accueil)
 
-function createGridCell(i,id, portrait, name, city, country, tagline, price, tags, mainContainer){
-    // Création d'une GridCell
-    var div_GridCell = document.createElement("div");
+function createGridCell(
+  i,
+  id,
+  portrait,
+  name,
+  city,
+  country,
+  tagline,
+  price,
+  tags,
+  mainContainer
+) {
+  // Boucle des liens
 
-    // Ajouts des Attributs
-    div_GridCell.className = "gridcell"
-    div_GridCell.setAttribute("role", "cell")
-    div_GridCell.ariaRowIndex = i+1;
+  var tag_links = ``;
+  for (var j = 0; j < tags.length; j++) {
+    tag_links += `
+        <a tabindex="${3 * i + 7}" href="#" onclick="test('${tags[j]}'")>
+            <p>#${tags[j]}</p>
+        </a>
+        `;
+  }
 
-    // Remplissage de la GridCell
-        // Création de la Premiere partie de la GridCell
-        var gridCellFirstPart = document.createElement("div");
+  // Ajout du HTML
 
-        // Ajouts des Attributs
-        gridCellFirstPart.ariaRowIndex = 5;
-
-        // Ajouts du Contenu
-            // Création du Portrait
-            var image = document.createElement("img");
-
-            // Ajouts des Attributs (Portrait)
-            image.src = "./images/Photographers ID Photos/" + portrait;
-            image.alt = "Portrait de " + name 
-
-            // Création du Titre (Nom)
-            var title_name = document.createElement("h2");
-
-            // Ajouts des Attributs (Nom)
-            title_name.innerHTML = name
-
-            // Placements des elements
-            gridCellFirstPart.appendChild(image);
-            gridCellFirstPart.appendChild(title_name);
-
-        // Placement de la Premiere partie dans sa GridCell
-        div_GridCell.appendChild(gridCellFirstPart);
-
-        // - * - * - * - * - * - SECOND PART OF GRID CELL - * - * - * - * - * - //
-
-        // Création de la Seconde partie de la GridCell
-        var gridCellSecondPart = document.createElement("div");
-
-        // Ajouts des Attributs
-        gridCellSecondPart.ariaRowIndex = 6;
-
-        // Ajouts du Contenu
-            // Création de la Localisation
-            var location = document.createElement("p");
-
-            // Ajouts des Attributs (Localisation)
-            location.className = "location";
-            location.innerHTML = city + ", " + country
-
-            // Création de la Tagline
-            var sentence = document.createElement("p");
-
-            // Ajouts des Attributs (Tagline)
-            sentence.className = "tagline"
-            sentence.innerHTML = tagline
-
-            // Création du prix
-            var cost = document.createElement("p");
-
-            // Ajouts des Attributs (prix)
-            cost.className = "price"
-            cost.innerHTML = price + "€/jour"
-
-            // Placement des elements
-            gridCellSecondPart.appendChild(location);
-            gridCellSecondPart.appendChild(sentence);
-            gridCellSecondPart.appendChild(cost);
-
-
-        // Placement de la Seconde partie dans sa GridCell
-        div_GridCell.appendChild(gridCellSecondPart);
-
-
-        // - * - * - * - * - * - THIRD PART OF GRID CELL - * - * - * - * - * - //
-
-        // Création de la Troisième partie de la GridCell
-        var gridCellThirdPart = document.createElement("div");
-
-        // Ajouts des Attributs
-        gridCellThirdPart.ariaRowIndex = 7;
-        gridCellThirdPart.className = "tags";
-
-        // Ajouts du Contenu
-            // Création des liens
-            for (var i = 0; i < tags.length; i++) {
-                // Création du lien
-                var tag_link = document.createElement("a")
-
-                // Ajouts des Attributs (Tagline)
-                tag_link.tabIndex = 7;
-                tag_link.href = "#";
-
-                    // Création du Contenu du lien
-                    var tag = document.createElement("p")
-
-                    // Ajouts des Attributs (Tagline)
-                    tag.innerHTML = "#" + tags[i];
-
-                    // Placement du tag
-                    tag_link.appendChild(tag);
-
-                // Placement du Lien
-                gridCellThirdPart.appendChild(tag_link);
-
-            }
-
-        // Placement de la Seconde partie dans sa GridCell
-        div_GridCell.appendChild(gridCellThirdPart);
-
-    // Placement de la GridCell
-    mainContainer.appendChild(div_GridCell);
+  mainContainer.innerHTML += `
+        <div class="gridcell" role="cell" aria-rowindex="${i + 1}">
+            <div tabindex="${3 * i + 5}">
+                <a href="#" class="lien_photographes lien_${portrait}">
+                    <img src="./images/Photographers ID Photos/${portrait}" alt="Portrait de ${name}">
+                    <h2>${name}</h2>
+                </a>
+            </div>
+            <div tabindex="${3 * i + 6}">
+                <p class="location">${city}, ${country}</p>
+                <p class="tagline">${tagline}</p>
+                <p class="price">${price}/jour</p>
+            </div>
+            <div tabindex="${3 * i + 7}" class="tags">
+            ${tag_links}
+            </div>
+        </div>
+        `;
 }
--- js/script.js
+++ js/script.js
@@ -1,35 +1,63 @@
 // Récupération du Json
-fetch('./js/data.json')
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                appendData(data);
-            })
-            .catch(function (err) {
-                console.log('error: ' + err);
-            });
+
+let jsondata;
+fetch("./js/data.json")
+  .then(function (data) {
+    return data.json();
+  })
+  .then(function (data) {
+    jsondata = data;
+    createHomePageHtml(data);
+  })
+  .catch(function (err) {
+    console.log("error: " + err);
+  });
 
 // Utilisation du Json
 
-        function appendData(data) {
-            // Cherche la grid
-            var mainContainer = document.getElementById("photographers_grid");
-            // Pour TOUT les photographes on crée une Gridcell avec leur contenu
-            for (var i = 0; i < data.photographers.length; i++) { 
-                createGridCell(
-                    i,
-                    data.photographers[i].id, 
-                    data.photographers[i].portrait,
-                    data.photographers[i].name, 
-                    data.photographers[i].city,
-                    data.photographers[i].country,
-                    data.photographers[i].tagline,
-                    data.photographers[i].price,
-                    data.photographers[i].tags,
-                    mainContainer
-                )
-            }
-        }
+// Création du html page d'accueil
 
-        
+function createHomePageHtml(data) {
+  // Cherche la grid
+  var mainContainer = document.getElementById("photographers_grid");
+  mainContainer.innerHTML = ``;
+  // Pour TOUT les photographes on crée une Gridcell avec leur contenu
+  for (var i = 0; i < data.photographers.length; i++) {
+    createGridCell(
+      i,
+      data.photographers[i].id,
+      data.photographers[i].portrait,
+      data.photographers[i].name,
+      data.photographers[i].city,
+      data.photographers[i].country,
+      data.photographers[i].tagline,
+      data.photographers[i].price,
+      data.photographers[i].tags,
+      mainContainer
+    );
+  }
+}
+
+// Filtre JSON
+function test(tag) {
+  // Cherche la grid
+  var mainContainer = document.getElementById("photographers_grid");
+  mainContainer.innerHTML = ``;
+
+  for (var i = 0; i < jsondata.photographers.length; i++) {
+    if (jsondata.photographers[i].tags.includes(tag)) {
+      createGridCell(
+        i,
+        jsondata.photographers[i].id,
+        jsondata.photographers[i].portrait,
+        jsondata.photographers[i].name,
+        jsondata.photographers[i].city,
+        jsondata.photographers[i].country,
+        jsondata.photographers[i].tagline,
+        jsondata.photographers[i].price,
+        jsondata.photographers[i].tags,
+        mainContainer
+      );
+    }
+  }
+}
